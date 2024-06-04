b1b77ceb0394f0afff92da9786a3e994a0e72eaa
loicmaes <production@maesloic.fr>
2024-06-04 15:17:54 +0200

    :wrench: Configure iconoir provider

--- app.vue
+++ app.vue
@@ -1,5 +1,15 @@
+<script setup lang="ts">
+import {IconoirProvider} from "@iconoir/vue";
+</script>
+
 <template>
-  <NuxtLayout>
-    <NuxtPage />
-  </NuxtLayout>
+  <IconoirProvider :icon-props="{
+    'width': '1.5rem',
+    'height': '1.5rem',
+    'stroke-width': '2',
+  }">
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </IconoirProvider>
 </template>
