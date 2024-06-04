7213ccb6e1e735dc4acac45289e43e8888a82518
loicmaes <production@maesloic.fr>
2024-06-04 18:01:47 +0200

    :rocket: Add the default page of the application

---
+++ pages/app/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import {useState} from "#imports";
+import type {IUser} from "~/types/IUser";
+
+const user = useState<IUser>('user');
+if (!user.value) navigateTo('/auth/login');
+</script>
+
+<template>
+  <p>Hello {{user?.firstName}} {{ user?.lastName }}</p>
+</template>
+
+<style scoped lang="sass">
+
+</style>
