1880ffd2eaf63a8bebb9caa70da5374be43e5757
loicmaes <production@maesloic.fr>
2024-06-04 13:36:43 +0200

    :rocket: Setup default layout and home page

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { Button } from "~/components/ui/button";
+</script>
+
+<template>
+  <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col gap-8">
+    <h1 class="text-4xl font-extrabold">Hello world!</h1>
+    <Button>Say hi 👋</Button>
+  </div>
+</template>
+
+<style scoped lang="sass">
+
+</style>
