14fabddba7fd58e3842d7e2d7979c97771bb5789
loicmaes <97891718+loicmaes@users.noreply.github.com>
2024-06-05 02:22:33 +0200

    :rocket: Add toaster component

--- app.vue
+++ app.vue
@@ -1,17 +1,18 @@
 <script setup lang="ts">
 import {IconoirProvider} from "@iconoir/vue";
-import {type IUser} from "~/types/IUser";
 import {useUser} from "~/composables/useAuth";
+import Toast from "~/components/ui/toast/Toaster.vue";
 
 const nuxtApp = useNuxtApp();
 nuxtApp.hook("page:finish", () => {
   window.scrollTo(0, 0);
 })
 
-const user: IUser = await useUser();
+await useUser();
 </script>
 
 <template>
+  <Toast />
   <IconoirProvider :icon-props="{
     'width': '1.5rem',
     'height': '1.5rem',
