6ceda97fb837b703468e45095848be156b517984
loicmaes <97891718+loicmaes@users.noreply.github.com>
2024-06-05 18:32:01 +0200

    :rocket: Setup navigation bar in default layout

---
+++ components/layout/default/navigation.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import {Button} from "~/components/ui/button";
+import User from "~/components/layout/user.vue";
+</script>
+
+<template>
+  <header class="nav">
+    <p>TickTack</p>
+
+    <nav>
+      <Button variant="link" as-child><NuxtLink to="/">Home</NuxtLink></Button>
+      <Button variant="link" as-child><NuxtLink to="/">Pricing</NuxtLink></Button>
+      <Button variant="link" as-child><NuxtLink to="/">About</NuxtLink></Button>
+    </nav>
+
+    <User />
+  </header>
+</template>
+
+<style scoped lang="sass">
+.nav
+  @apply px-40 py-6 w-full flex items-center justify-between
+</style>
