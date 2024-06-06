9bc91d8562b8498cd5b90208dc31bb7a084f00ed
loicmaes <production@maesloic.fr>
2024-06-06 19:21:32 +0200

    :rocket: Temporarily push wip work

--- components/layout/default/navigation.vue
+++ components/layout/default/navigation.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import {Button} from "~/components/ui/button";
 import User from "~/components/layout/user.vue";
+import {Dashboard} from "@iconoir/vue";
+import {DropdownMenuGroup, DropdownMenuItem} from "~/components/ui/dropdown-menu";
 </script>
 
 <template>
@@ -13,11 +15,25 @@ import User from "~/components/layout/user.vue";
       <Button variant="link" as-child><NuxtLink to="/">About</NuxtLink></Button>
     </nav>
 
-    <User />
+    <User>
+      <template #content>
+        <DropdownMenuGroup>
+          <DropdownMenuItem class="item" as-child>
+            <NuxtLink to="/app">
+              <Dashboard class="icon" />
+              <span>Dashboard</span>
+            </NuxtLink>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+      </template>
+    </User>
   </header>
 </template>
 
 <style scoped lang="sass">
+.icon
+  @apply mr-2 h-4 w-4
+
 .nav
   @apply px-40 py-6 w-full flex items-center justify-between
 </style>
