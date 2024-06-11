1be1ee0bc1808ebc703e0b0fb2e00e6e144bc886
loicmaes <production@maesloic.fr>
2024-06-11 20:33:58 +0200

    :rocket: Add work session viewer page

---
+++ pages/app/time-tracking/[sessionUid].vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import {useRoute} from "#imports";
+import type {IWorkSession} from "~/types/timeTracking/IWorkSession";
+import {useWorkSession} from "~/composables/useTimeTracking";
+import PageHeader from "~/components/layout/app/page/pageHeader.vue";
+
+definePageMeta({
+  layout: 'app-base'
+});
+useHead({
+  title: `TickTack · Viewer`
+});
+
+const { params } = useRoute();
+const uid: string = params.sessionUid as string;
+const workSession = ref<IWorkSession | null>(await useWorkSession(uid));
+</script>
+
+<template>
+  <main data-page="workSessionViewer" class="page workSession">
+    <PageHeader :name="workSession?.name">
+
+    </PageHeader>
+  </main>
+</template>
+
+<style scoped lang="sass">
+@forward '../../../assets/css/shared/authPage'
+</style>
