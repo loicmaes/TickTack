8c2fe97cf96b8e94971dd3a7deff5221135cfb9f
loicmaes <97891718+loicmaes@users.noreply.github.com>
2024-06-13 10:44:07 +0200

    :ambulance: Fix column name issue

--- types/timeTracking/IWorkSessionStep.ts
+++ types/timeTracking/IWorkSessionStep.ts
@@ -6,6 +6,7 @@ export interface IWorkSessionStep {
   start: Date;
   end?: Date;
   elapsed: number;
-  type: WorkSessionStepType;
+  stepType: WorkSessionStepType;
+  status?: WorkSessionProgressionStatus;
   comment?: string;
 }
